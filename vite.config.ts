f903d3f8107f30c2f6144339afc9c02cef97e6ac
Arulmurugan <88280031+Arul0320@users.noreply.github.com>
2026-02-18 08:00:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio_main/', // 👈 This must match your GitHub repo name exactly
+  base: '/Personal_Portfolio/', 
   plugins: [react()],
 })
